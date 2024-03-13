a3315da2fae061320a4fbd80a85c83fb93e62e75
BlindArcher <robbe.decraemer@ucll.be>
2024-03-13 15:27:51 +0100

    Added fullscreen toggle

--- engine/main.js
+++ engine/main.js
@@ -91,6 +91,8 @@ playButton.onclick = async function () {
 
     window.requestAnimationFrame(updateTick);
 
+    document.getElementById("fullscreen").onclick = toggleFullscreen;
+
     var hidden = true;
     var button = document.getElementById("showdebug");
 
@@ -102,4 +104,38 @@ playButton.onclick = async function () {
 
     status.innerHTML = "Initialized";
     status.classList.add('hidden');
+}
+
+function toggleFullscreen() {
+    if (document.fullscreenElement) {
+        closeFullscreen();
+    }
+
+    else {
+        openFullscreen();
+    }
+}
+
+/* View in fullscreen */
+function openFullscreen() {
+    var elem = document.documentElement;
+
+    if (elem.requestFullscreen) {
+        elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+        elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+        elem.msRequestFullscreen();
+    }
+}
+
+/* Close fullscreen */
+function closeFullscreen() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) { /* Safari */
+        document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE11 */
+        document.msExitFullscreen();
+    }
 }
